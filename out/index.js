d97d013c7990917e8a790f49470dac0ed31315ca
Brian Di Palma <offler@gmail.com>
2014-05-24 14:35:26 +0100

    Initial commit of the spa 2014 header.

---
+++ out/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperties(exports, {
+  printHello: {get: function() {
+      return printHello;
+    }},
+  __esModule: {value: true}
+});
+function printHello() {
+  console.info("Hello");
+}
