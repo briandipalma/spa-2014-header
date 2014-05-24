9b8288be91e157e98d51bd70458a60ff469f43c5
Brian Di Palma <offler@gmail.com>
2014-05-24 16:31:31 +0100

    Add X-Tags custom element.
    Loading X-Tags and creating a simple X-Tags element.

--- out/index.js
+++ out/index.js
@@ -8,3 +8,22 @@ Object.defineProperties(exports, {
 function printHello() {
   console.info("Hello");
 }
+xtag.register('spa2014-header', {
+  lifecycle: {
+    created: function() {
+      console.info("header created");
+    },
+    inserted: function() {
+      console.info("header inserted");
+    },
+    removed: function() {
+      console.info("header removed");
+    },
+    attributeChanged: function() {
+      console.info("header attribute changed");
+    }
+  },
+  events: {},
+  accessors: {},
+  methods: {}
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 export function printHello() {
 	console.info("Hello");
-}
+}	
+// Create your component here
+// http://x-tags.org/docs
+
+xtag.register('spa2014-header', {
+	lifecycle: {
+	  created: function() {
+		  console.info("header created");
+	  },
+	  inserted: function() {
+		  console.info("header inserted");
+	  },
+	  removed: function() {
+		  console.info("header removed");
+	  },
+	  attributeChanged: function() {
+		  console.info("header attribute changed");
+	  }
+	},
+	events: {
+
+	},
+	accessors: {
+
+	},
+	methods: {
+
+	}
+});
