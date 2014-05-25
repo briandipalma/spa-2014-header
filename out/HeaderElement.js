a58f6c4c1de5eace1e6a02db7c1f2789918d36c3
Brian Di Palma <offler@gmail.com>
2014-05-25 21:29:41 +0100

    Compile as CommonJS.

--- out/HeaderElement.js
+++ out/HeaderElement.js
@@ -1,32 +1,22 @@
-System.register([], function($__0) {
-  "use strict";
-  var HeaderElement;
-  return {
-    exports: {
-      get HeaderElement() {
-        return HeaderElement;
-      },
-      set HeaderElement(value) {
-        HeaderElement = value;
-      }
-    },
-    execute: function() {
-      HeaderElement = (function($__super) {
-        var HeaderElement = function HeaderElement() {
-          $traceurRuntime.defaultSuperCall(this, HeaderElement.prototype, arguments);
-        };
-        return ($traceurRuntime.createClass)(HeaderElement, {
-          createdCallback: function() {
-            this.textContent = "I'm a header!";
-          },
-          attachedCallback: function() {},
-          detachedCallback: function() {},
-          attributeChangedCallback: function(attr, oldVal, newVal) {},
-          foo: function() {
-            console.log("foo() called");
-          }
-        }, {}, $__super);
-      }(HTMLElement));
-    }
-  };
+"use strict";
+Object.defineProperties(exports, {
+  HeaderElement: {get: function() {
+      return HeaderElement;
+    }},
+  __esModule: {value: true}
 });
+var HeaderElement = function HeaderElement() {
+  $traceurRuntime.defaultSuperCall(this, $HeaderElement.prototype, arguments);
+};
+var $HeaderElement = HeaderElement;
+($traceurRuntime.createClass)(HeaderElement, {
+  createdCallback: function() {
+    this.textContent = "I'm a header!";
+  },
+  attachedCallback: function() {},
+  detachedCallback: function() {},
+  attributeChangedCallback: function(attr, oldVal, newVal) {},
+  foo: function() {
+    console.log("foo() called");
+  }
+}, {}, HTMLElement);
