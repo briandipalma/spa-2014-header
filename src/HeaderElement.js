80e8156ef06d45a2936ea74844f5e4429ae25bd4
Brian Di Palma <offler@gmail.com>
2014-05-25 20:23:18 +0100

    Convert the HeaderElement to use ES6 classes.

--- src/HeaderElement.js
+++ src/HeaderElement.js
@@ -1,22 +1,19 @@
-// Creates an object based in the HTML Element prototype
-var HeaderPrototype = Object.create(HTMLElement.prototype);
+export class HeaderElement extends HTMLElement {
+	// Fires when an instance of the HeaderPrototype is created
+	createdCallback() {
+		this.textContent = "I'm a header!";
+	};
 
-// Fires when an instance of the HeaderPrototype is created
-HeaderPrototype.createdCallback = function() {
-	this.textContent = "I'm a header!";
-};
+	// Fires when an instance was inserted into the document
+	attachedCallback() {};
 
-// Fires when an instance was inserted into the document
-HeaderPrototype.attachedCallback = function() {};
+	// Fires when an instance was removed from the document
+	detachedCallback() {};
 
-// Fires when an instance was removed from the document
-HeaderPrototype.detachedCallback = function() {};
+	// Fires when an attribute was added, removed, or updated
+	attributeChangedCallback(attr, oldVal, newVal) {};
 
-// Fires when an attribute was added, removed, or updated
-HeaderPrototype.attributeChangedCallback = function(attr, oldVal, newVal) {};
-
-HeaderPrototype.foo = function() {
-	console.log("foo() called");
-};
-
-export default { prototype: HeaderPrototype };
+	foo() {
+		console.log("foo() called");
+	};
+}
