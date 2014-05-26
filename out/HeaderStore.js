f76e909b4f98f22b9ad950ef39d267a74e8a5c81
Brian Di Palma <offler@gmail.com>
2014-05-26 16:03:37 +0100

    Add a HeaderStore to the HeaderElement package.
    This is the Store of this component, Store as in the Flux pattern Store concept.
    Similar to a model, holding the state and business logic for the component.

---
+++ out/HeaderStore.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperties(exports, {
+  HeaderStore: {get: function() {
+      return HeaderStore;
+    }},
+  __esModule: {value: true}
+});
+var Emitr = $traceurRuntime.assertObject(require("emitr")).default;
+var HeaderStore = Object.assign({}, Emitr.prototype);
---
+++ src/HeaderStore.js
@@ -0,0 +1,3 @@
+import Emitr from "emitr";
+
+export var HeaderStore = Object.assign({}, Emitr.prototype);
