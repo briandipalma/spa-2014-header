0d14df2bab629de5f00544f25bdbc1b5780b35d3
Brian Di Palma <offler@gmail.com>
2014-05-27 19:55:03 +0100

    Implement the HeaderStore.
    Complete most of the functionality of the HeaderStore.

--- out/HeaderStore.js
+++ out/HeaderStore.js
@@ -6,4 +6,56 @@ Object.defineProperties(exports, {
   __esModule: {value: true}
 });
 var Emitr = $traceurRuntime.assertObject(require("emitr")).default;
-var HeaderStore = Object.assign({}, Emitr.prototype);
+var HeaderConstants = $traceurRuntime.assertObject(require("./HeaderConstants")).default;
+var HeaderDispatcher = $traceurRuntime.assertObject(require("./HeaderDispatcher")).default;
+var CHANGE_EVENT = 'change';
+var headerState = {
+  loginState: "logged-out",
+  loginErrorState: "no-error"
+};
+function setHeaderState(loginState, loginErrorState) {
+  headerState.loginState = loginState;
+  headerState.loginErrorState = loginErrorState;
+}
+var HeaderStore = Object.assign({
+  getState: function() {
+    return headerState;
+  },
+  handleDispatcherAction: function(payload) {
+    var action = payload.action;
+    switch (action.actionType) {
+      case HeaderConstants.LOGGED_IN:
+        setHeaderState("logged-in", "no-error");
+        break;
+      case HeaderConstants.LOGGING_IN:
+        setHeaderState("logging-in", "no-error");
+        break;
+      case HeaderConstants.LOGGED_OUT:
+        setHeaderState("logged-out", "no-error");
+        break;
+      case HeaderConstants.LOGGING_OUT:
+        setHeaderState("logging-out", "no-error");
+        break;
+      case HeaderConstants.LOGIN_ERROR:
+        setHeaderState("logged-out", "error");
+        break;
+      case HeaderConstants.LOGIN_ERROR_CLEARED:
+        headerState.loginErrorState = "no-error";
+        break;
+      default:
+        return true;
+    }
+    this.emitChange();
+    return true;
+  },
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  }
+}, Emitr.prototype);
+HeaderDispatcher.register(HeaderStore.handleDispatcherAction);
--- src/HeaderStore.js
+++ src/HeaderStore.js
@@ -1,3 +1,74 @@
 import Emitr from "emitr";
 
-export var HeaderStore = Object.assign({}, Emitr.prototype);
+import HeaderConstants from "./HeaderConstants";
+import HeaderDispatcher from "./HeaderDispatcher";
+
+var CHANGE_EVENT = 'change';
+
+var headerState = {
+    loginState: "logged-out",
+    loginErrorState: "no-error"
+};
+
+function setHeaderState(loginState, loginErrorState) {
+    headerState.loginState = loginState;
+    headerState.loginErrorState = loginErrorState;
+}
+
+export var HeaderStore = Object.assign({
+    getState() {
+        return headerState;
+    },
+
+    handleDispatcherAction(payload) {
+        var action = payload.action;
+
+        switch (action.actionType) {
+            case HeaderConstants.LOGGED_IN:
+                setHeaderState("logged-in", "no-error");
+                break;
+            case HeaderConstants.LOGGING_IN:
+                setHeaderState("logging-in", "no-error");
+                break;
+            case HeaderConstants.LOGGED_OUT:
+                setHeaderState("logged-out", "no-error");
+                break;
+            case HeaderConstants.LOGGING_OUT:
+                setHeaderState("logging-out", "no-error");
+                break;
+            case HeaderConstants.LOGIN_ERROR:
+                setHeaderState("logged-out", "error");
+                break;
+            case HeaderConstants.LOGIN_ERROR_CLEARED:
+                headerState.loginErrorState = "no-error";
+                break;
+            default:
+                return true;
+        }
+
+        this.emitChange();
+
+        return true;
+    },
+
+    emitChange() {
+        this.emit(CHANGE_EVENT);
+    },
+
+    /**
+     * @param {function} callback
+     */
+    addChangeListener(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+
+    /**
+     * @param {function} callback
+     */
+    removeChangeListener(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    }
+}, Emitr.prototype);
+
+// Register to handle all updates
+HeaderDispatcher.register(HeaderStore.handleDispatcherAction);
