7bed9be9e50dd7a1f13a209ae93f682b697669ce
Brian Di Palma <offler@gmail.com>
2014-05-28 09:14:50 +0100

    Adding HeaderConstants.

---
+++ out/HeaderConstants.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperties(exports, {
+  default: {get: function() {
+      return $__default;
+    }},
+  __esModule: {value: true}
+});
+var $__default = {
+  LOGGED_IN: null,
+  LOGGING_IN: null,
+  LOGGED_OUT: null,
+  LOGGING_OUT: null,
+  LOGIN_ERROR: null,
+  LOGIN_ERROR_CLEARED: null
+};
---
+++ src/HeaderConstants.js
@@ -0,0 +1,8 @@
+export default {
+    LOGGED_IN: null,
+    LOGGING_IN: null,
+    LOGGED_OUT: null,
+    LOGGING_OUT: null,
+    LOGIN_ERROR: null,
+    LOGIN_ERROR_CLEARED: null
+}
