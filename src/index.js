5afa59148eceac307f1d3c6095653e36c0b2eca4
Brian Di Palma <offler@gmail.com>
2014-06-10 20:02:27 +0100

    Do not export function to create store and actions
    New flux-es6 contains this method.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
-import {Dispatcher} from "flux-es6";
-
-import HeaderStore from "./HeaderStore";
-import HeaderActions from "./HeaderActions";
-
-export function createStoreAndActions() {
-    var headerStore = new HeaderStore();
-    var headerDispatcher = new Dispatcher();
-    var headerActions = new HeaderActions(headerDispatcher);
-    
-    headerDispatcher.register((payload) => headerStore.handleDispatcherAction(payload));
-
-    return [headerStore, headerActions];
-}
-
-export {HeaderElement} from "./HeaderElement";
+export {HeaderElement} from './HeaderElement';
 export {HeaderConstants} from './HeaderConstants';
